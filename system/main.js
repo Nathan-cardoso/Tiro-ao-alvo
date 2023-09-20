229c68caa5f584dbdcca3f3246e8b59f17af848e
Sr-Cardoso <nathancar.linhares77@gmail.com>
2023-09-20 13:02:01 -0300

    feat: passando a cena para uma função

--- system/main.js
+++ system/main.js
@@ -1,16 +1,42 @@
 import * as THREE from 'three';
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.5, 1000 );
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+document.body.appendChild( renderer.domElement )
 
 
-function animete(){
-    requestAnimationFrame(animete);
-    renderer.render(scene, camera);
+
+  sceneInit();
+  animate();
+
+  function sceneInit(){
+
+    const worldWidth = 128, worldDepth = 128;
+    
+    camera.position.y = 200;
+    
+      scene.background = new THREE.Color(0xaaccff);
+      scene.fog = new THREE.FogExp2(0xaaccff, 0.0007);
+    
+      const geometry = new THREE.PlaneGeometry(20000, 2000, worldWidth - 1, worldDepth - 1);
+      geometry.rotateX(-Math.PI / 2);
+    
+      const texture = new THREE.TextureLoader().load('textures/water.jpg');
+      texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+      texture.repeat.set(5, 5);
+    
+    
+      const material = new THREE.MeshBasicMaterial({ color: 0x0044ff, map: texture });
+    
+      const mesh = new THREE.Mesh(geometry, material);
+      scene.add(mesh);
+    }
+
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
 }
 
-animete()
